fb8556c23f5b441842ae3dfda8de211c15f4bae7
Shivanshu Sharma <shivanshu1177@chitkara.edu.in>
2022-01-01 19:44:29 +0530

    q1

---
+++ Basics/LecqueOne.java
@@ -0,0 +1,10 @@
+class LecqueOne{
+    public static void main(String[] args) {
+        int a, b, sum;
+        a = 10;
+        b = 5;
+        sum = a + b;
+
+        System.out.println(sum);
+    }
+}
