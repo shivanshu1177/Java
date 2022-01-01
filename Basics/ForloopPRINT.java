7388ec99bf42a475040d95c3ecc4c20b18ba4f1f
Shivanshu Sharma <shivanshu1177@chitkara.edu.in>
2022-01-01 21:58:58 +0530

    one to 10 print

---
+++ Basics/ForloopPRINT.java
@@ -0,0 +1,10 @@
+public class ForloopPRINT {
+
+    public static void main(String[] args) {
+        int n;
+        for (n = 0; n < 11; n++) {
+            System.out.println(n);
+
+        }
+    }
+}
