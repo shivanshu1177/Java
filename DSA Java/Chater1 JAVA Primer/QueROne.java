75762ba602eb13b5717ee8b847cc2398c852a51e
Shivanshu Sharma <shivanshu1177@chitkara.edu.in>
2022-01-05 20:35:45 +0530

    DSA book

---
+++ DSA Java/Chater1 JAVA Primer/QueROne.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class QueROne {
+
+    public static void main(String[] args) {
+        //take input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Integer: ");
+
+        int a = sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.println("String: ");
+
+        String b = sc.nextLine();
+
+        System.out.println("Float: ");
+
+        float c = sc.nextFloat();
+
+        // print 
+
+        System.out.println(a);
+
+        System.out.println(b);
+
+        System.out.println(c);
+
+
+
+        
+
+        
+    }
+}
--- DSA Java/Solution.java
+++ DSA Java/Solution.java
@@ -1,18 +1,24 @@
 import java.io.*;
+import java.util.*;
+import java.text.*;
 import java.math.*;
-
-
+import java.util.regex.*;
 
 public class Solution {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
 
-        int n = sc.nextInt();
-
-        for (int i = 1; i <= 10; i++) {
-            
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String month = in.next();
+        String day = in.next();
+        String year = in.next();
+        String input_date = day + "/" + month + "/" + year;
+        SimpleDateFormat format1 = new SimpleDateFormat("dd/MM/yyyy");
+        try 
+        {
+            DateFormat format2 = new SimpleDateFormat("EEEE"); 
+            String final_day = format2.format(format1.parse(input_date));
+            System.out.println(final_day.toUpperCase());
         }
-        
-
+        catch(Exception e){}
     }
 }
