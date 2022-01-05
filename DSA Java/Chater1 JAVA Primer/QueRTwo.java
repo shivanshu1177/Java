21eee8ffefb93bfb69f478773256e8fb41facd03
Shivanshu Sharma <shivanshu1177@chitkara.edu.in>
2022-01-05 21:03:32 +0530

    .

---
+++ DSA Java/Chater1 JAVA Primer/QueRTwo.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class QueRTwo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int [] A = new int[n];
+        
+
+        for (int i = 0; i < n; i++) {
+            A[i] = sc.nextInt();
+            
+        }
+        int [] B = A.clone();
+
+        System.out.println(Arrays.toString(B));
+    }
+    
+}
+//this was an verbel problem this profgram only takes arry input 
