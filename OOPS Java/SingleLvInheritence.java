689c3ebd8c5dab58b4f102be6f81a6b8e9d8e69d
Shivanshu Sharma <shivanshu1177@chitkara.edu.in>
2022-01-12 23:10:10 +0530

    Oops

---
+++ OOPS Java/SingleLvInheritence.java
@@ -0,0 +1,24 @@
+class Shape{
+    public void area() {
+        System.out.println("Display area");
+        
+    }
+
+}
+class Triangle extends Shape{
+    public void area(int l, int h) {
+        System.out.println(1/2*l*h);
+        
+    }
+
+}
+
+
+
+
+public class SingleLvInheritence {
+    public static void main(String[] args) {
+        
+    }
+    
+}
