9d25cdeba4c01614e87a9162964fc65eae6c8b9b
Shivanshu Sharma <shivanshu1177@chitkara.edu.in>
2022-01-12 23:21:09 +0530

    Oops

---
+++ OOPS Java/HierchialInheritence.java
@@ -0,0 +1,30 @@
+class Shapea{
+    public void area() {
+        System.out.println("Display area");
+        
+    }
+
+}
+class Trianglea extends Shapea{
+    public void area(int l, int h) {
+        System.out.println(1/2*l*h);
+        
+    }
+
+}
+class Circle extends Shapea{
+    public void area(float r) {
+        System.out.println(3.14*r*r);
+        
+    }
+}
+
+
+
+
+public class HierchialInheritence {
+    public static void main(String[] args) {
+        
+    }
+    
+}
